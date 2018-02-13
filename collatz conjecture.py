88abda0d69e0233b760c86a13b3f41153f6ec773
nualagallagher <36246540+nualagallagher@users.noreply.github.com>
2018-02-13 21:57:34 +0000

    Create collatz conjecture.py

---
+++ collatz conjecture.py
@@ -0,0 +1,15 @@
+# nuala gallagher collatz number
+def collatz(number):
+
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+n = int(input("Enter a number: "))
+while n != 1:
+    n = collatz(int(n))
